1c966ffb50e796a54176ba4a1cf653f5b3dabd1f
Richard Rowland <richard@entwinesolutions.co.uk>
2025-05-14 23:08:04 +0100

    Modified helloeWorld to clarify _name__ == __main

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,10 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def printName():
+    print("Hello World!")
+    name = input("What is your name my man?")
+    print("Nice to meet you", name)
 
+if __name__ == "__main__":
+    printName()
